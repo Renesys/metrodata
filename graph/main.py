505b3bdb0696e0088cd0b5bef10278f4aaff16a9
renesys <hrs350x@gmail.com>
2021-08-23 21:50:25 +0900

    add trend by year

--- graph/main.py
+++ graph/main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 def year_trend(df, year):
@@ -30,7 +31,22 @@ def total_trend(df):
     plt.xticks(rotation=45)
     plt.show()
 
-# Press the green button in the gutter to run the script.
+def total_trend_by_month(df):
+    df = df.sort_values(by=['Year', 'Month'])
+    res = sns.barplot(x="Month", y="Data", data=df, hue="Year",
+                      estimator=np.sum,
+                      palette='pastel')
+    for p in res.patches:
+        if not pd.isna(p.get_height()):
+            val = str(round(int(p.get_height()),-6))[:-6]+"M"
+        else:
+            val = 0
+        res.annotate(val, (p.get_x() + p.get_width()/2, p.get_height()+30),\
+                     ha="center", va="center", fontsize=10, color="black", xytext=(0,10),\
+                     rotation=90, textcoords="offset points")
+    plt.legend(bbox_to_anchor=(1.02,1), loc='center right', borderaxespad=0)
+    plt.show()
+
 if __name__ == '__main__':
     pd.set_option('display.width', None)
     df = pd.read_csv('defined_metro.csv', dtype=str)
@@ -39,14 +55,15 @@ if __name__ == '__main__':
         df[key] = df[key].astype(typename)
 
     print("1. 전체 이용량")
-    print("2. 특정년도 월별 이용량")
+    print("2. 월별 전체 이용량")
+    print("3. 특정년도 월별 이용량")
     selection = int(input("검색 : "))
     if selection == 1:
         total_trend(df)
     elif selection == 2:
+        total_trend_by_month(df)
+    elif selection == 3:
         year = str(input("연도 입력 : "))
         year_trend(df, year)
 
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
