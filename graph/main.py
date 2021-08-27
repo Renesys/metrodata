c15291019c3aeaf46f3e6d5128d4e2868097071e
renesys <hrs350x@gmail.com>
2021-08-28 00:50:43 +0900

    fix total trend part

--- graph/main.py
+++ graph/main.py
@@ -4,25 +4,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def year_trend(df, year):
-    sub = df[df['Year'] == year]
-    sub = sub.sort_values(by='Month')
-    sns.set_theme(style='whitegrid', palette='Set2')
-    res = sns.barplot(x='Month', y='Data', data=sub, estimator=np.sum, ci=None)
-    for p in res.patches:
-        if not pd.isna(p.get_height()):
-            val = str(round(int(p.get_height()),-6))[:-6]+"M"
-        else:
-            val = 0
-        res.annotate(val, (p.get_x() + p.get_width()/2, p.get_height()),\
-                     ha="center", va="center", fontsize=10, color="black", xytext=(0,10),\
-                     textcoords="offset points")
-    plt.show()
-
-def total_trend(df):
+def total_trend_bk(df):
     df['YearMonth'] = df['Year'] + df['Month']
     df = df.sort_values(by=['YearMonth'])
-    sns.set_theme(style='whitegrid', palette='deep')
+    sns.set_theme(style='whitegrid')
+    sns.set_palette("Set2", 7)
     res = sns.barplot(x='YearMonth', y ='Data', data=df, estimator=np.sum, ci=None)
     for p in res.patches:
         val = str(round(int(p.get_height()),-6))[:-6]+"M"
@@ -30,6 +16,22 @@ def total_trend(df):
                      ha="center", va="center", fontsize=10, color="black", xytext=(2,15),\
                      rotation=90, textcoords="offset points")
     plt.xticks(rotation=45)
+    plt.show()
+
+def total_trend(df):
+    df = df.sort_values(by=['Year', 'Month'])
+    sns.set_theme(style='whitegrid', palette='Paired')
+    res = sns.barplot(x="Year", y="Data", data=df, hue="Month",
+                      estimator=np.sum, ci=None)
+    for p in res.patches:
+        if not pd.isna(p.get_height()):
+            val = str(round(int(p.get_height()),-6))[:-6]+"M"
+        else:
+            val = 0
+        res.annotate(val, (p.get_x() + p.get_width()/2, p.get_height()),\
+                     ha="center", va="center", fontsize=10, color="black", xytext=(2,16),\
+                     rotation=90, textcoords="offset points")
+    plt.legend(bbox_to_anchor=(1,1.05))
     plt.show()
 
 def total_trend_by_month(df):
@@ -48,6 +50,22 @@ def total_trend_by_month(df):
     plt.legend(bbox_to_anchor=(1,1.05))
     plt.show()
 
+def year_trend(df, year):
+    sub = df[df['Year'] == year]
+    sub = sub.sort_values(by='Month')
+    sns.set_theme(style='whitegrid', palette='Set2')
+    res = sns.barplot(x='Month', y='Data', data=sub, estimator=np.sum, ci=None)
+    for p in res.patches:
+        if not pd.isna(p.get_height()):
+            val = str(round(int(p.get_height()),-6))[:-6]+"M"
+        else:
+            val = 0
+        res.annotate(val, (p.get_x() + p.get_width()/2, p.get_height()),\
+                     ha="center", va="center", fontsize=10, color="black", xytext=(0,10),\
+                     textcoords="offset points")
+    plt.show()
+
+
 if __name__ == '__main__':
     pd.set_option('display.width', None)
     df = pd.read_csv('defined_metro.csv', dtype=str)
@@ -55,21 +73,16 @@ if __name__ == '__main__':
     for key, typename in types.items():
         df[key] = df[key].astype(typename)
 
-    while True:
-        print("1. 전체 이용량")
-        print("2. 월별 전체 이용량")
-        print("3. 특정년도 월별 이용량")
-        print("0. 종료")
-        selection = int(input("검색 : "))
-        if selection == 1:
-            total_trend(df)
-        elif selection == 2:
-            total_trend_by_month(df)
-        elif selection == 3:
-            year = str(input("연도 입력 : "))
-            year_trend(df, year)
-        elif selection == 0:
-            break
-        print('\n')
+    print("1. 전체 이용량")
+    print("2. 월별 전체 이용량")
+    print("3. 특정년도 월별 이용량")
+    selection = int(input("검색 : "))
+    if selection == 1:
+        total_trend(df)
+    elif selection == 2:
+        total_trend_by_month(df)
+    elif selection == 3:
+        year = str(input("연도 입력 : "))
+        year_trend(df, year)
 
 
