b015845575f64ed2087df743e2b0d02e61859456
renesys <hrs350x@gmail.com>
2021-08-28 23:23:35 +0900

    add station data search. need to improve

--- graph/main.py
+++ graph/main.py
@@ -4,20 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def total_trend_bk(df):
-    df['YearMonth'] = df['Year'] + df['Month']
-    df = df.sort_values(by=['YearMonth'])
-    sns.set_theme(style='whitegrid')
-    sns.set_palette("Set2", 7)
-    res = sns.barplot(x='YearMonth', y ='Data', data=df, estimator=np.sum, ci=None)
-    for p in res.patches:
-        val = str(round(int(p.get_height()),-6))[:-6]+"M"
-        res.annotate(val, (p.get_x() + p.get_width()/2, p.get_height()),\
-                     ha="center", va="center", fontsize=10, color="black", xytext=(2,15),\
-                     rotation=90, textcoords="offset points")
-    plt.xticks(rotation=45)
-    plt.show()
-
 def total_trend(df):
     df = df.sort_values(by=['Year', 'Month'])
     sns.set_theme(style='whitegrid', palette='Paired')
@@ -53,7 +39,7 @@ def total_trend_by_month(df):
 def year_trend(df, year):
     sub = df[df['Year'] == year]
     sub = sub.sort_values(by='Month')
-    sns.set_theme(style='whitegrid', palette='Set2')
+    sns.set_theme(style='whitegrid', palette='Paired')
     res = sns.barplot(x='Month', y='Data', data=sub, estimator=np.sum, ci=None)
     for p in res.patches:
         if not pd.isna(p.get_height()):
@@ -65,6 +51,18 @@ def year_trend(df, year):
                      textcoords="offset points")
     plt.show()
 
+def daily_trend(df, sta):
+    sub = df[df['Station'] == sta]
+    sub = sub.sort_values(by=['Hour'])
+    print(sub.head(10))
+    fig, ax = plt.subplots(figsize=(15, 5))
+    #plt.rcParams['font.family'] = 'Malgun Gothic'
+    #plt.rcParams['axes.unicode_minus'] = False
+    sns.set(font="Malgun Gothic", rc={"axes.unicode_minus":False}, style='whitegrid')
+    pt = sns.lineplot(x="Hour", y="Data", data=sub, hue="Type", ax=ax)
+    pt.set_title("{}역 시간대별 승하차량 평균".format(sta))
+    plt.show()
+
 
 if __name__ == '__main__':
     pd.set_option('display.width', None)
@@ -73,16 +71,24 @@ if __name__ == '__main__':
     for key, typename in types.items():
         df[key] = df[key].astype(typename)
 
-    print("1. 전체 이용량")
-    print("2. 월별 전체 이용량")
-    print("3. 특정년도 월별 이용량")
-    selection = int(input("검색 : "))
-    if selection == 1:
-        total_trend(df)
-    elif selection == 2:
-        total_trend_by_month(df)
-    elif selection == 3:
-        year = str(input("연도 입력 : "))
-        year_trend(df, year)
+    while True:
+        print("1. 전체 이용량")
+        print("2. 월별 전체 이용량")
+        print("3. 특정년도 월별 이용량")
+        print("4. 특정 역 시간대별 승하차량 추세")
+        print("0. 종료")
+        selection = int(input("검색 : "))
+        if selection == 1:
+            total_trend(df)
+        elif selection == 2:
+            total_trend_by_month(df)
+        elif selection == 3:
+            year = str(input("연도 입력 : "))
+            year_trend(df, year)
+        elif selection == 4:
+            sta = input("역 이름 : ")
+            daily_trend(df, sta)
+        elif selection == 0:
+            break
 
 
